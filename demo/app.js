b5903995e0b0c94119222f66dfe685008d7433a5
Sandor Turanszky <sandor.turanszky@racebets.com>
2015-03-25 10:49:03 +0100

    angular-directive-implementing-float-label-pattern

---
+++ demo/app.js
@@ -0,0 +1,31 @@
+var app = angular.module('floatingLabelApp', []);
+
+app.controller('MainCtrl', function($scope) {
+    $scope.user = {};
+});
+
+app.directive('withFloatingLabel', function () {
+    return {
+        restrict: 'A',
+        link: function ($scope, $element, attrs) {
+            var template = '<div class="floating-label">' + attrs.placeholder +'</div>';
+
+            //append floating label template
+            $element.after(template);
+
+            //remove placeholder
+            $element.removeAttr('placeholder');
+
+            //hide label tag assotiated with given input
+            document.querySelector('label[for="' +  attrs.id +  '"]').style.display = 'none';
+
+            $scope.$watch(function () {
+                if($element.val().toString().length < 1) {
+                    $element.addClass('empty');
+                } else {
+                    $element.removeClass('empty');
+                }
+            });
+        }
+    };
+});
